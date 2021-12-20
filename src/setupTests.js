89ef7f7ecf489b7ba7a5b0686ff2dd6c11308488
Leonardo S Coelho <leonardo_soares29@hotmail.com>
2021-12-20 10:47:48 +0100

    Removing promise from setupTest

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,15 +4,6 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 const mockGeolocation = {
-  getCurrentPosition: jest.fn().mockImplementationOnce((success) =>
-    Promise.resolve(
-      success({
-        coords: {
-          latitude: 51.1,
-          longitude: 45.3,
-        },
-      })
-    )
-  ),
+  getCurrentPosition: jest.fn(),
 };
 global.navigator.geolocation = mockGeolocation;
